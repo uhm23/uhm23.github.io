a5f1e25a5643123b0a1cf3c166d8bf1dd598d155
unknown <ultimatehypermachine@gmail.com>
2023-09-21 07:32:31 +0100

    4th Commit: 'Documentation' edits

--- AsciiArtEels/script.js
+++ AsciiArtEels/script.js
@@ -1,3 +1,23 @@
+/*
+
+version <0.1
+
+The code for this is based on the code in the tutorial video:
+ASCII Art with Vanilla JavaScript by Franks Laboratory.
+
+https://www.youtube.com/watch?v=HeT-5RZgEQY
+
+The original version is a static effect.
+I added the rudimentary animation and my own text strings instead of symbols.
+
+This isn't great code, but I really like experimenting with graphics code.
+
+ToDo:
+
+Document the algorithm.
+
+Optimise the code
+*/
 const canvas = document.getElementById('canvas1');
 const ctx = canvas.getContext('2d');
 
